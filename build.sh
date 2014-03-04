634f8350f178f75c820232cdaf2bfd11ae31b583
Scott Davis <stdavis@utah.gov>
2014-03-04 07:32:06 -0700

    Refined startup.
    Create `App` widget programmatically.
    Added dist/index.html
    Removed dojoConfig and moved config into run.js

--- build.sh
+++ build.sh
@@ -12,8 +12,11 @@ TOOLSDIR="$SRCDIR/util/buildscripts"
 # Destination directory for built code
 DISTDIR="$BASEDIR/dist"
 
-# Path to the main application dojo configuration
-LOADERCONF="$SRCDIR/dojoConfig.js"
+# Module ID of the main application package loader configuration
+LOADERMID="app/run"
+
+# Main application package loader configuration
+LOADERCONF="$SRCDIR/$LOADERMID.js"
 
 # Main application package build configuration
 PROFILE="$BASEDIR/profiles/build.profile.js"
@@ -24,9 +27,9 @@ PROFILE="$BASEDIR/profiles/build.profile.js"
 cd "$TOOLSDIR"
 
 if which node >/dev/null; then
-  node ../../dojo/dojo.js load=build --dojoConfig "$LOADERCONF" --profile "$PROFILE" --releaseDir "$DISTDIR" $@
+  node ../../dojo/dojo.js load=build --require "$LOADERCONF" --profile "$PROFILE" --releaseDir "$DISTDIR" $@
 elif which java >/dev/null; then
-  java -Xms256m -Xmx256m  -cp ../shrinksafe/js.jar:../closureCompiler/compiler.jar:../shrinksafe/shrinksafe.jar org.mozilla.javascript.tools.shell.Main  ../../dojo/dojo.js baseUrl=../../dojo load=build --dojoConfig "$LOADERCONF" --profile "$PROFILE" --releaseDir "$DISTDIR" $@
+  java -Xms256m -Xmx256m  -cp ../shrinksafe/js.jar:../closureCompiler/compiler.jar:../shrinksafe/shrinksafe.jar org.mozilla.javascript.tools.shell.Main  ../../dojo/dojo.js baseUrl=../../dojo load=build --require "$LOADERCONF" --profile "$PROFILE" --releaseDir "$DISTDIR" $@
 else
   echo "Need node.js or Java to build!"
   exit 1
