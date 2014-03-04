8cd351f71ce20068a44f5ea9b8198df4505aa5df
Scott Davis <stdavis@utah.gov>
2014-03-04 07:35:23 -0700

    Layer file working except for widget templates.

--- profiles/build.profile.js
+++ profiles/build.profile.js
@@ -1,5 +1,17 @@
 var profile = {
     basePath: '../src/',
     action: 'release',
-    optimize: 'closure'
+    optimize: 'closure',
+    selectorEngine: 'acme',
+    layers: {
+        'dojo/dojo': {
+            include: [
+                'dojo/i18n',
+                'app/run',
+                'dojo/text'
+            ],
+            customBase: true,
+            boot: true
+        }
+    }
 };
